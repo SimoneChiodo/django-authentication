fbf2cd3c8c722b5c3cbe8ee135ee073856f2fb1e
SimoneChiodo <simochi.005@gmail.com>
2025-10-21 19:04:23 +0200

    Add old pages, Django Default Form

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
-urlPatterns = [
-  path('', views.home, name='home')
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('<int:post_id>/', views.postDetail, name='post_detail'),
+    path('new/', views.postCreate, name="post_create"),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post
+from .forms import PostForm
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
-  return render(request, 'index.html')
+  posts = Post.objects.all().order_by('-data_creazione')
+  return render(request, "index.html", { "posts": posts })
+
+def postDetail(request, post_id):
+  post = get_object_or_404(Post, id=post_id)
+  return render(request, "postDetail.html", {"post": post})
+
+def postCreate(request):
+  if request.method == "POST":
+    form = PostForm(request.POST)
+    if form.is_valid():
+      form.save()
+      messages.success(request, "Post creato con successo!")
+      return redirect('home') 
+  else:
+    form = PostForm()
+  
+  return render(request, "postCreate.html", {"form": form})
